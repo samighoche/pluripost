8925430d44a779b70bb3d693a6f457b711d97ac0
andrewmalek27 <andrewmalek@me.com>
2015-04-06 12:15:20 -0400

    Initial Commit
    Basic Storyboard Skeleton

---
+++ PluriPost/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PluriPost
+//
+//  Created by Andrew Moussa Malek on 4/5/15.
+//  Copyright (c) 2015 PluriPost. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func pressedButton(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
